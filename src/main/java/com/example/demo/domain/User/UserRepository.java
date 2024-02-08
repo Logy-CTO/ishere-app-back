81abf2650fe834b7b05c1df88c91cfb0a25bd7c3
Logy-CTO <hong2hwa58@gmail.com>
2024-02-09 02:02:35 +0900

    초기 세팅

--- src/main/java/com/example/demo/domain/User/UserRepository.java
+++ src/main/java/com/example/demo/domain/User/UserRepository.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:src/main/java/com/example/demo/domain/User/UserRepository.java
 package com.example.demo.domain.User;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,18 +9,4 @@ public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByPhoneNumber(String phoneNumber);
     boolean existsByUserName(String userName);
     User findByPhoneNumber(String phoneNumber);
-=======
-package com.example.demo.User;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import java.util.Optional;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-    boolean existsByPhoneNumber(String phoneNumber);
-    boolean existsByUserName(String userName);
-    User findByPhoneNumber(String phoneNumber);
-
->>>>>>> b6b88a1f87b098b9cc0f0df6c91bb3b67b71b298:src/main/java/com/example/demo/User/UserRepository.java
 }
