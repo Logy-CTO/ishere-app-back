db29d6e93e28aff58f248e02f457e4e3e405b863
Logy-CTO <hong2hwa58@gmail.com>
2024-02-29 17:45:12 +0900

    2.29 refresh컨트롤러 수정 header

--- src/main/java/com/example/demo/global/controller/RefreshController.java
+++ src/main/java/com/example/demo/global/controller/RefreshController.java
@@ -1,6 +1,7 @@
 package com.example.demo.global.controller;
 
 import com.example.demo.global.security.service.JwtService;
+import org.apache.tomcat.util.http.parser.Authorization;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,6 +28,11 @@ public class RefreshController {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Refresh Token이 만료되었습니다.");
         }
 
-        return ResponseEntity.status(HttpStatus.OK).body("Refresh Token이 유효합니다.");
+        // 수정: AccessToken을 Response Header에 추가
+        String accessToken = map.get("accessToken");
+        return ResponseEntity.status(HttpStatus.OK)
+                .header("Authorization", accessToken)
+                .body("Refresh Token이 유효합니다.");
     }
 }
+
